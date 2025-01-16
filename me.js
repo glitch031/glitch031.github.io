f9a3a15132fc22a54d9b2745155eac2bc087f89c
Craig Hanks <61710896+glitch031@users.noreply.github.com>
2025-01-15 23:13:50 -0600

    adding menu to other page

---
+++ me.js
@@ -0,0 +1,18 @@
+
+function dropMenu(x) {
+    x.classList.toggle("change");
+    document.getElementById("menuDropdown").classList.toggle("show");
+  
+  }
+  window.onclick = function(event) {
+      if (!event.target.matches('.menuDrop')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }

d9d7fdb27b1e0c56f700b59ffd4ce46560976a45
Craig Hanks <61710896+glitch031@users.noreply.github.com>
2025-09-03 23:24:14 -0500

    last one
    home page was broke

--- js/projects.js
+++ js/projects.js
@@ -1,16 +1,22 @@
+
 function dropMenu(x) {
   x.classList.toggle("change");
   document.getElementById("menuDropdown").classList.toggle("show");
+
 }
-window.onclick = function (event) {
-  if (!event.target.matches(".menuDrop")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
+// Prevent window.onclick from closing the dropdown when clicking the button
+document.querySelector('.menuDrop').addEventListener('click', function(event) {
+  event.stopPropagation();
+});
+window.onclick = function(event) {
+    if (!event.target.matches('.menuDrop')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
       }
     }
   }
-};
